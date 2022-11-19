8bfc5949e0117c3be54348c39dfc5479a9f838b5
pranjala3 <pranjal.kishore@datastax.com>
2022-11-19 23:29:10 +0530

    First Commit

--- src/main/java/AstraQuery.java
+++ src/main/java/AstraQuery.java
@@ -1,10 +1,12 @@
 
 import com.datastax.oss.driver.api.core.CqlSession;
 import com.datastax.oss.driver.api.core.cql.PreparedStatement;
+import com.datastax.oss.driver.api.core.cql.ResultSet;
+import com.datastax.oss.driver.api.core.cql.Row;
 
 import java.nio.file.Paths;
 
-public class MumbaiAstra {
+public class AstraQuery {
 
     static final String ASTRA_ZIP_FILE = "/Users/pranjal/Documents/Astra-Project/MumbaiDatabasse-Bundle/secure-connect-mydb.zip";
     static final String ASTRA_USERNAME = "ytKEklPjxdFeOAQPufPJTmxl";
@@ -19,10 +21,11 @@ public class MumbaiAstra {
         System.out.println("Initiating Connection to Astra Cluster");
         System.out.println(java.time.LocalDateTime.now());
         CqlSession cqlSession = CqlSession.builder()
-                .withCloudSecureConnectBundle(Paths.get(ASTRA_ZIP_FILE))
-                .withAuthCredentials(ASTRA_USERNAME, ASTRA_PASSWORD)
-                .withKeyspace(ASTRA_KEYSPACE)
-                .build();
+                    .withCloudSecureConnectBundle(Paths.get(ASTRA_ZIP_FILE))
+                    .withAuthCredentials(ASTRA_USERNAME, ASTRA_PASSWORD)
+                    .withKeyspace(ASTRA_KEYSPACE)
+                    .build();
+
         // logger.info("[OK] Welcome to ASTRA. Connected to Keyspace {}", cqlSession.getKeyspace().get());
 
         // logger.info("[OK] Success");
@@ -32,22 +35,32 @@ public class MumbaiAstra {
         System.out.println(java.time.LocalDateTime.now());
 
         PreparedStatement ps1 = cqlSession.prepare("insert into myks.t1 (id,city,name) values (?,?,?)");
-        PreparedStatement ps2 = cqlSession.prepare("insert into myks.t2 (id,city,name) values (?,?,?)");
-        PreparedStatement ps3 = cqlSession.prepare("insert into myks.t3 (id,city,name) values (?,?,?)");
+    //    PreparedStatement ps2 = cqlSession.prepare("insert into myks.t2 (id,city,name) values (?,?,?)");
+    //    PreparedStatement ps3 = cqlSession.prepare("insert into myks.t3 (id,city,name) values (?,?,?)");
 
-        for(int i = 0; i < 50000 ; i++ )
+    //    PreparedStatement qs1 = cqlSession.prepare("select * from myks.t1 WHERE id = ? and city = ?");
+        PreparedStatement qs1 = cqlSession.prepare("select * from myks.t1 WHERE id > ? and id < ? allow filtering");
+
+        for(int i = 2100; i < 40000 ; i++ )
         {
-            id = i;
-            city = "this is city"+ i;
-            name = "Pranjal - "+ i;
-            cqlSession.execute(ps1.bind(id,city,name));
-            cqlSession.execute(ps2.bind(id,city,name));
-            cqlSession.execute(ps3.bind(id,city,name));
-            if((i%1000) == 0)
-            {
+
+      //      if((i%1000) == 0)
+      //      {
+
+                city = "this is city"+ i;
                 System.out.println(java.time.LocalDateTime.now());
-                System.out.println("Look, I have written " + String.valueOf(i) + " records by now!");
-            }
+                System.out.println("Reading now");
+       //         ResultSet rs = cqlSession.execute(qs1.bind(i,city));
+                ResultSet rs = cqlSession.execute(qs1.bind(i,i+50));
+                for (Row row : rs) {
+                System.out.println(row.getFormattedContents());
+                 }
+           //     Row row = rs.one();
+              //  System.out.println(rs.one());
+         //   System.out.format("%s %d\n", rs.getString("city"), rs.getInt("id"));
+
+      //      }
+
         }
 
     }
